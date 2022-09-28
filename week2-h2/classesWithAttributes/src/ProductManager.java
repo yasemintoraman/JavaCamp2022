0435a632920bb4444ff5956d79187d12679650f2
U190709023 <yasemintoraman@posta.mu.edu.tr>
2022-09-28 20:31:57 +0300

    homework2 completed

---
+++ week2-h2/classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,8 @@
+public class ProductManager {
+    //ürünle ilgili operasyonlar içerir
+    public void Add(Product product){
+        //JDBC
+        System.out.println("Ürün eklendi " + product.getName());
+    }
+
+}
